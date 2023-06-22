556dc5f36fff046bedb4ebf5dd30b946f3e906a9
Angel Leon <angel@leanware.io>
2023-06-22 17:48:15 -0500

    ask verification layout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Role } from '@app/models';
 import { AccountService } from '@app/services';
+import {
+  ConfirmDialogData,
+  ConfirmDialogComponent,
+} from '@app/shared/components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +13,38 @@ import { AccountService } from '@app/services';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-  constructor(private accountService: AccountService) {}
+  Role = Role;
+
+  constructor(
+    private accountService: AccountService,
+    private dialog: MatDialog
+  ) {}
 
   get account() {
     return this.accountService.accountValue;
+  }
+
+  openConfirmDialog(): void {
+    const dialogData: ConfirmDialogData = {
+      title: 'Ask for verification',
+      message: 'Are you sure? verification costs $5',
+      confirmText: 'Confirmar',
+      cancelText: 'Cancelar',
+    };
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '400px',
+      data: dialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // Acción cuando se confirma
+        console.log('Confirmado');
+      } else {
+        // Acción cuando se cancela
+        console.log('Cancelado');
+      }
+    });
   }
 }
